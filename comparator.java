0e23bb23aea9d2725ddb6f4dcac1e1929a4be858
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-09-08 19:08:09 +0530

    Update comparator.java

--- comparator.java
+++ comparator.java
@@ -8,10 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;        
 import java.util.Collections;
 import java.util.Comparator;
-/**
- *
- * @author ARUMUGASAMY
- */
+
 public class comparator {
     public static void main(String[] args) {
        
